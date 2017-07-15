e5912e9d10185a5bd8f37b708c9853d4bc9e219e
Todd Isaacs <teisaacs21w@gmail.com>
2017-07-15 10:21:42 -0400

    added additional data model to support categories, locations and rooms

---
+++ MyStuff/ItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemTableViewCell.swift
+//  MyStuff
+//
+//  Created by Isaacs, Todd on 7/14/17.
+//  Copyright © 2017 Isaacs, Todd. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
