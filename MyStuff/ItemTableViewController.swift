f8752619cf00d5147728adf060524a7798ff34b9
Todd Isaacs <teisaacs21w@gmail.com>
2017-07-29 11:11:56 -0400

    hooked item view up to core data with NSFetchedResultsController

--- MyStuff/ItemTableViewController.swift
+++ MyStuff/ItemTableViewController.swift
@@ -7,11 +7,11 @@
 //
 
 import UIKit
+import CoreData
 
 class ItemTableViewController: UITableViewController {
-
-  var items = ["item1", "item2"]
   
+  var fetchedResultsController:NSFetchedResultsController<Item>!
   var coreDataStack:CoreDataStack!
   
   //MARK: Actions
@@ -24,108 +24,138 @@ class ItemTableViewController: UITableViewController {
   
   
   
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-      
-      
+    let sort = NSSortDescriptor(key: #keyPath(Item.name), ascending: true, selector: #selector(NSString.caseInsensitiveCompare))
+    fetchRequest.sortDescriptors = [sort]
+    
+    
+    fetchedResultsController = NSFetchedResultsController(
+      fetchRequest: fetchRequest,
+      managedObjectContext: coreDataStack.managedContext,
+      sectionNameKeyPath: nil,
+      cacheName: nil)
+    
+    fetchedResultsController.delegate = self
+    
+    do {
+      try fetchedResultsController.performFetch()
+    } catch let error as NSError {
+      print("Fetching error: \(error), \(error.userInfo)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return items.count
-    }
+  }
 
   
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as! ItemTableViewCell
-
-      if indexPath.row == 1 {
-        cell.itemName.text = "Samsung Color Laser Printer"
-        cell.itemValue.text = "$45.00"
-        cell.itemDescription.text = "not working well"
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    guard let sections = fetchedResultsController.sections else {
+      return 0
+    }
+    
+    return sections.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let sectionInfo = fetchedResultsController.sections?[section] else {
+      return 0
+    }
+    
+    return sectionInfo.numberOfObjects
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ItemTableViewCell", for: indexPath) as! ItemTableViewCell
+    
+    configure(cell: cell, for: indexPath)
+    
+    return cell
+  }
+  
+  func configure(cell: UITableViewCell, for indexPath: IndexPath) {
+    guard let cell = cell as? ItemTableViewCell else {
+      return
+    }
+    
+    let item = fetchedResultsController.object(at: indexPath)
+    cell.itemName.text = item.name
+    
+    if let value = item.itemValue {
+      cell.itemValue.text = NumberFormatter.localizedString(from: value, number: .currency)
+    }
+    
+    cell.itemDescription.text = item.descr
+  }
+  
+  /*
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+   */
+  
+  /*
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+   */
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    super.prepare(for: segue, sender: sender)
+    
+    switch(segue.identifier ?? "") {
+    case "addItem" :
+      guard let navController = segue.destination as? UINavigationController else {
+        fatalError("Unexpected destination \(segue.destination), Expected UINavigationCOntroller")
       }
       
-
-        return cell
-    }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-  
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-      super.prepare(for: segue, sender: sender)
+      guard let addItemController = navController.topViewController as? AddItemViewController else {
+        fatalError("Unexpected destination Expected AddItemViewController")
+      }
       
-      switch(segue.identifier ?? "") {
-      case "addItem" :
-        guard let navController = segue.destination as? UINavigationController else {
-           fatalError("Unexpected destination \(segue.destination), Expected UINavigationCOntroller")
-        }
-        
-        guard let addItemController = navController.topViewController as? AddItemViewController else {
-          fatalError("Unexpected destination Expected AddItemViewController")
-        }
-        
-        //populate core data stack
-        addItemController.delegate = self
-        addItemController.item = Item(context: coreDataStack.managedContext)
-      default:
-        fatalError("Unexpected segue identifier \(String(describing: segue.identifier))")
-        
+      //populate core data stack
+      addItemController.delegate = self
+      addItemController.item = Item(context: coreDataStack.managedContext)
+    default:
+      fatalError("Unexpected segue identifier \(String(describing: segue.identifier))")
+      
     }
   }
 }
@@ -140,4 +170,31 @@ extension ItemTableViewController: AddItemViewControllerDelegate {
       print("Error saving data \(error)")
     }
   }
+}
+
+extension ItemTableViewController: NSFetchedResultsControllerDelegate {
+  func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    //let tableview know we are going to update
+    tableView.beginUpdates()
+  }
+  
+  func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+    switch type {
+    case .insert:
+      tableView.insertRows(at: [newIndexPath!], with: .automatic)
+    case .delete:
+      tableView.deleteRows(at: [indexPath!], with: .automatic)
+    case .update:
+      let cell = tableView.cellForRow(at: indexPath!) as! ItemTableViewCell
+      configure(cell: cell, for: indexPath!)
+    case .move:
+      tableView.deleteRows(at: [indexPath!], with: .automatic)
+      tableView.insertRows(at: [newIndexPath!], with: .automatic)
+    }
+  }
+  
+  func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+    tableView.endUpdates()
+  }
+  
 }
